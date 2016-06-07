8ca25c59639e838b1eb9d5016dda0c87959a7ba8
haha370104 <137368440@qq.com>
2016-06-07 10:46:38 +0800

    解析原理放进json

--- tools/ICBC.py
+++ tools/ICBC.py
@@ -1,7 +1,22 @@
 import requests
 import json
 from model.bank_product import bank_product, db
-import re
+
+
+def ICBC_parse(product):
+    f = open('ICBC.json')
+    dic = json.load(f)
+    columns = {}
+    for key in dic.keys():
+        if dic[key]['complex']:
+            args = []
+            for arg in dic[key]['value']['key']:
+                args.append(product[arg])
+            fun = eval(dic[key]['value']['function'])
+            columns[key] = fun(args)
+        else:
+            columns[key] = product[dic[key]['value']]
+    return columns
 
 
 def update():
@@ -13,28 +28,16 @@ def update():
         product += json.loads(req.text)
     count = 0
     for pr_json in product:
-        rate = float(re.findall('[0-9\.]+', pr_json['intendYield'])[0].rstrip('%'))
-        duration = pr_json['productTerm']
-        if duration == '无固定期限':
-            duration = 0
-        else:
-            duration = int(re.findall('[0-9]', duration)[0])
-        duration_flag = False
-        url = 'http://www.icbc.com.cn/icbc/%E7%BD%91%E4%B8%8A%E7%90%86%E8%B4%A2/%E7%90%86%E8%B4%A2%E4%BA%A7%E5%93%81/%E4%BA%A7%E5%93%81%E9%A2%84%E8%A7%88.htm?productId=' + \
-              pr_json['prodID']
-        if pr_json['categoryL2'] == '6':
-            if pr_json['productName'].find('美元') != -1:
-                currencies = '美元'
-            elif pr_json['productName'].find('欧元') != -1:
-                currencies = '欧元'
-            else:
-                currencies = '其他'
-        else:
-            currencies = '人民币'
-        if bank_product.query.get(pr_json['prodID']) == None:
-            p = bank_product(pr_json['prodID'], pr_json['productName'], rate, duration, duration_flag, None, url,
-                             int(float(pr_json['buyPaamt'])), '中国工商银行', currencies)
+        result = ICBC_parse(pr_json)
+        if bank_product.query.get(result['product_ID']) == None:
+            p = bank_product(result['product_ID'], result['product_name'], result['interest_rate'],
+                             result['duration'], result['duration_flag'], result['type_flag'], result['product_url'],
+                             result['start_amount'], result['bank_name'], result['currencies'])
             db.session.add(p)
             count += 1
     db.session.commit()
+
     return {'update_count': count}
+
+
+print(update())
